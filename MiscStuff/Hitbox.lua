fce593b389dab0c405be1fc6c6cc7320d1567dbe
Mr Wompington <riviera.alpacas_0f@icloud.com>
2025-09-05 22:33:04 -0400

    Update Hitbox.lua

--- MiscStuff/Hitbox.lua
+++ MiscStuff/Hitbox.lua
@@ -1,7 +1,7 @@
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
-local targetPartName = "Head"
+local targetPartName = "HumanoidRootPart"
 local targetParts = {}
 
 local size = {
