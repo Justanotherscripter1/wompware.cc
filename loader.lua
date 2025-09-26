c0a29bcebf352615859b16931a3d1e8a6541d1a4
Mr Wompington <riviera.alpacas_0f@icloud.com>
2025-09-26 18:10:11 -0400

    dunno what table.string.find is but i removed it :D

--- loader.lua
+++ loader.lua
@@ -1,15 +1,15 @@
---this does NOT contain the key systewm, because if it did bypasses could be done, the key system is per script. This just loads the script for your game or the universal one.
+local HttpService = game:GetService("HttpService")
+
 local games = {
-    ["4991214437"] = "https://raw.githubusercontent.com/Justanotherscripter1/wompware.cc/refs/heads/main/Versions/town.lua", --town and td are litteraly the exact same game
-    ["6652350934"] = "https://raw.githubusercontent.com/Justanotherscripter1/wompware.cc/refs/heads/main/Versions/town.lua", --town and td are litteraly the exact same game
+    ["4991214437"] = "https://raw.githubusercontent.com/Justanotherscripter1/wompware.cc/refs/heads/main/Versions/town.lua",
+    ["6652350934"] = "https://raw.githubusercontent.com/Justanotherscripter1/wompware.cc/refs/heads/main/Versions/town.lua",
     ["Universal"]  = "https://raw.githubusercontent.com/Justanotherscripter1/wompware.cc/refs/heads/main/Versions/Universal.lua"
 }
 
 local function getScriptUrlForGame(placeId)
-    return games[tostring(placeId)]
+    return games[tostring(placeId)] or games["Universal"]
 end
-if table.string.find(games,tostring(game.PlaceId) , 1, false) then 
-loadstring(getScriptUrlForGame(game.PlaceId))
-else
-loadstring(getScriptUrlForGame("Universal"))
-end
+
+local url = getScriptUrlForGame(game.PlaceId)
+local scriptContent = game:HttpGet(url)
+loadstring(scriptContent)()
